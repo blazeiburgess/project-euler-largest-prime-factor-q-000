8075e35f8acb97b8d29dbbb4abc7c1f1a5f8ff5c
thedailyephemera <thedailyephemera@gmail.com>
2016-04-30 11:23:16 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,12 @@
-# Enter your procedural solution here!
+# your procedural solution here!
+def factor(value)
+	(2..(Math.sqrt(value))).select {|num| value % num == 0}.empty?
+end
+
+def largest_prime_factor(limit)
+	return ((2..(Math.sqrt(limit))).select {|num| limit % num == 0 && factor(num)}).max
+end
+
+puts largest_prime_factor(50)
+puts largest_prime_factor(13195)
+puts largest_prime_factor(600851475143)
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,12 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+	def initialize(limit)
+		@limit = limit
+	end
+	def factor(value)
+		(2..(Math.sqrt(value))).select {|num| value % num == 0}.empty?
+	end
+	def number
+		return ((2..(Math.sqrt(@limit))).select {|num| @limit % num == 0 && factor(num)}).max
+	end
+end
